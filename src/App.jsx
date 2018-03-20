6f66416744365acd2ced45b396f669cd83ce7d24
Antonio Pugliese <apugliese74@gmail.com>
2018-03-20 16:38:05 -0400

    refactored clock into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 import './App.css';
 
 class App extends Component {
@@ -17,13 +18,10 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <div className='App-title'>Countdown to {this.state.deadline}</div>
-        <div>
-          <div className='Clock-days'>14 days</div>
-          <div className='Clock-hours'>30 hours</div>
-          <div className='Clock-minutes'>15 minutes</div>
-          <div className='Clock-seconds'>20 seconds</div>
+        <div className='App-title'>
+          Countdown to {this.state.deadline}
         </div>
+        <Clock />
         <div>
           <input
             placeholder='new date'
